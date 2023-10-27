473b045a669a1f04c5c2eb6d4fe93494f74a9df9
Иванов Алексей Александрович <ivanovaa@mir-omsk.ru>
2023-10-27 22:28:23 +0600

    front

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,13 +1,13 @@
 /* eslint-disable */
 export default {
-  displayName: 'org',
+  displayName: 'frontend',
   preset: './jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: './coverage/org',
+  coverageDirectory: './coverage/frontend',
   testMatch: [
     '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
     '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
