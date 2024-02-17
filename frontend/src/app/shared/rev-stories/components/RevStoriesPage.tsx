e001b580f19d532774aef38c1301359810e30004
ivanovaa <ivanovaa@mir-omsk.ru>
2024-02-17 13:20:55 +0600

    remove tree

--- frontend/src/app/shared/rev-stories/components/RevStoriesPage.tsx
+++ frontend/src/app/shared/rev-stories/components/RevStoriesPage.tsx
@@ -5,15 +5,8 @@ import { StoriesContent } from './StoriesContent';
 
 export const RevStoriesPage = () => {
   return (
-    <Stack direction="row" height="100%">
-      <Box width={400} padding={1}>
-        <StoriesTree />
-      </Box>
-      <Box padding={1} width="100%">
-        <Routes>
-          <Route path="/:repoId/:branchId" element={<StoriesContent />}></Route>
-        </Routes>
-      </Box>
-    </Stack>
+    <Routes>
+      <Route path="/:repoId/:branchId" element={<StoriesContent />}></Route>
+    </Routes>
   );
 };
